d6c3a133a585bcdb007d90ed8734bb34ab51a117
RotColton <maromidomingo@gmail.com>
2025-05-11 16:07:09 +0200

    fix(#1): Edit "import.sh" file to prevent duplicate imports by checking collection document count

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-mongoimport --host mongodb --db nyc --collection restaurants --type json --file /mongo-seed/restaurants.json
+COUNT=$(mongo --quiet --host mongodb --eval "db.getSiblingDB('nyc').restaurants.countDocuments()")
+
+if [ "$COUNT" -eq 0 ]; then
+  echo  "No documents found. Importing data..."
+  mongoimport --host mongodb --db nyc --collection restaurants --type json --file /mongo-seed/restaurants.json
+else
+  echo "The collection already contains data. Skipping import."
+fi
